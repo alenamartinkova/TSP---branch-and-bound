4d92b946a035d71ac71b64d3b1f1f237202e5b65
alenamartinkova <martinkova.a@gmail.com>
2022-10-27 09:19:09 +0200

    tralala 33/33

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from Coordinate import Coordinate
 from typing import List
 import itertools as it
 import multiprocessing as mp
+import time
 
 
 def open_file():
+    """
+    Function that opens and reads file
+    :return:
+    """
     coords_list_l = []
     with open("/Users/alenamartinkova/Desktop/School/Ing./2. rok/PA/TSP/data.txt") as f:
         lines_after_7 = list(f.readlines())[7:]
@@ -17,6 +22,11 @@ def open_file():
 
 
 def create_matrix(input_list: List[Coordinate]):
+    """
+    Function that creates matrix of distances from coordinates
+    :param input_list:
+    :return:
+    """
     output_matrix = []
     row_size = column_size = len(input_list)
 
@@ -35,58 +45,82 @@ def create_matrix(input_list: List[Coordinate]):
     return output_matrix
 
 
-def evaluate_bnb(distances, perm, n, best_found):
+def get_value_or_index(distances, perm, n, best):
+    """
+    Function that counts distance for route
+    :param distances:
+    :param perm:
+    :param n:
+    :param best:
+    :return:
+    """
     result = 0
+    route = ''
     for i in range(n - 1):
         result += distances[perm[i]][perm[i + 1]]
-        if result >= best_found:
-            return False, i
+        route += str(perm[i])
+        if result >= best:
+            return False, i, route
 
     result = result + distances[perm[-1]][perm[0]]
-    return True, result
+    return True, result, route
 
 
-def perms_prefix_bnb(distances, n, s):
-    perm = [a for a in range(n) if a != s]
-    best_found = 100000000
+def branch_and_bound(matrix_distances, n, s):
+    """
+    Function that counts branch and bound for root
+    :param matrix_distances:
+    :param n:
+    :param s:
+    :return:
+    """
+    permutations = [a for a in range(n) if a != s]
+    best = 9999999
     skip = False
-    val = 0
+    value = 0
     current = 0
+    best_route = ''
 
-    for p in it.permutations(perm):
-        perm = [s]
-        perm.extend(p)
+    for permutation in it.permutations(permutations):
+        permutations = [s]
+        permutations.extend(permutation)
 
         if skip:
-            if perm[val] == current:
+            if permutations[value] == current:
                 continue
             else:
                 skip = False
-                success, val = evaluate_bnb(distances, perm, n, best_found)
+                success, value, route = get_value_or_index(matrix_distances, permutations, n, best)
         else:
-            success, val = evaluate_bnb(distances, perm, n, best_found)
+            success, value, route = get_value_or_index(matrix_distances, permutations, n, best)
 
         if success:
-            if val < best_found:
-                best_found = val
+            if value < best:
+                best = value
+                best_route = route
         else:
-            current = perm[val]
+            current = permutations[value]
             skip = True
 
-    return best_found
+    best_route = best_route + best_route[0]
+
+    return best, best_route
 
 
 def main():
+    start = time.time()
     coords_list = open_file()
     x = 10
     coords_list = coords_list[:x]
     matrix = create_matrix(coords_list)
-    Is = list(range(0, x))
+    range_list = list(range(0, x))
 
-    with mp.Pool(processes=9) as pool:
-        ret = pool.starmap(perms_prefix_bnb, zip(it.repeat(matrix), it.repeat(x), Is))
+    with mp.Pool(processes=1) as pool:
+        ret = pool.starmap(branch_and_bound, zip(it.repeat(matrix), it.repeat(x), range_list))
 
     print(ret)
+    end = time.time()
+    print(end - start)
 
 
 if __name__ == '__main__':
